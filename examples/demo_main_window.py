fe3e19a5c0add4bdbc3f9a2a10bf14e148b8e9e5
Dorian CARDOSO <doriancardoso00@gmail.com>
2025-07-27 02:28:05 +0200

    style(demo): custom line edit

--- examples/demo_main_window.py
+++ examples/demo_main_window.py
@@ -1,17 +1,20 @@
-from PySide6.QtWidgets import QHBoxLayout, QWidget, QVBoxLayout
+from PySide6.QtWidgets import QHBoxLayout, QWidget, QVBoxLayout, QSizePolicy
 
 from qute.components.buttons.custom_pushbuttons import CustomPushButton
 from qute.components.containers.custom_container_widget import CustomContainerWidget
 from qute.components.containers.custom_groupbox import CustomGroupBox
 from qute.components.containers.custom_label import CustomLabel
 from qute.components.containers.custom_mainwindow import CustomMainWindow
+from qute.components.inputs.custom_lineedit import CustomLineEdit
 
 
 class DemoMainWindow(CustomMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Qute Demo App")
-        self.resize(500, 400)
+        self.resize(900, 750)
+        size_policy = QSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Expanding)
+        self.setSizePolicy(size_policy)
 
         self.main_h_layout = QHBoxLayout(self.central_widget)
 
@@ -26,6 +29,9 @@ class DemoMainWindow(CustomMainWindow):
 
         self.standard_widget_v_layout.addStretch()
 
+        self.standard_lineedit = CustomLineEdit()
+        self.standard_widget_v_layout.addWidget(self.standard_lineedit)
+
         self.theme_button = CustomPushButton("Switch app theme")
         self.standard_widget_v_layout.addWidget(self.theme_button)
 
@@ -35,6 +41,9 @@ class DemoMainWindow(CustomMainWindow):
 
         self.embedded_groupbox = CustomGroupBox("Embedded group box example")
         self.custom_widget_v_layout.addWidget(self.embedded_groupbox)
+
+        self.embedded_lineedit = CustomLineEdit()
+        self.custom_widget_v_layout.addWidget(self.embedded_lineedit)
 
         self.custom_widget_v_layout.addStretch()
 
@@ -47,5 +56,5 @@ class DemoMainWindow(CustomMainWindow):
         self.main_h_layout.addWidget(self.standard_widget)
         self.main_h_layout.addWidget(self.custom_widget)
 
-        self.main_h_layout.setStretch(0, 2)
+        self.main_h_layout.setStretch(0, 4)
         self.main_h_layout.setStretch(1, 1)
---
+++ qute/components/inputs/custom_lineedit.py
@@ -0,0 +1,8 @@
+from PySide6.QtWidgets import QLineEdit
+
+
+class CustomLineEdit(QLineEdit):
+    def __init__(self):
+        super().__init__()
+
+        self.setObjectName("custom_lineedit")
